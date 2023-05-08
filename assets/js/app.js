cfbf9fc5d37456467b2b272a3873f064732fe4c3
Jay <killerdrug007@gmail.com>
2023-05-08 19:12:34 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,25 @@
 (function ($) {
-	'use strict';
-
-	$(function () {
-	});
+    'use strict';
+    const handleSliderHero = function () {
+        if ($('#slider-hero').length) {
+            new Swiper('#slider-hero .swiper', {
+                speed: 500,
+                slidesPerView: 1,
+                preloadImages: false,
+                effect: 'fade',
+                loop: true,
+                autoplay: {
+                    delay: 8000,
+                    disableOnInteraction: false,
+                },
+                navigation: {
+                    nextEl: "#slider-hero .slider-button_next",
+                    prevEl: "#slider-hero .slider-button_prev",
+                },
+            });
+        }
+    }
+    $(function () {
+        handleSliderHero();
+    });
 })(jQuery);
