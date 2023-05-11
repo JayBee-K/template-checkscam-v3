09729fa013210b89df9e052f99914a49cdc76892
Jay <killerdrug007@gmail.com>
2023-05-11 19:26:13 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,23 +3,35 @@
     const handleSliderHero = function () {
         if ($('#slider-hero').length) {
             new Swiper('#slider-hero .swiper', {
-                speed: 500,
-                slidesPerView: 1,
-                preloadImages: false,
-                effect: 'fade',
-                loop: true,
-                autoplay: {
-                    delay: 8000,
-                    disableOnInteraction: false,
-                },
-                navigation: {
-                    nextEl: "#slider-hero .slider-button_next",
-                    prevEl: "#slider-hero .slider-button_prev",
+                speed: 500, slidesPerView: 1, preloadImages: false, effect: 'fade', loop: true, autoplay: {
+                    delay: 8000, disableOnInteraction: false,
+                }, navigation: {
+                    nextEl: "#slider-hero .slider-button_next", prevEl: "#slider-hero .slider-button_prev",
                 },
             });
         }
     }
+
+    let initClipboardCopy = function (value) {
+        let createTextarea = document.createElement('textarea');
+        createTextarea.style.cssText = 'position: absolute; left: -99999px';
+        createTextarea.setAttribute("id", "textareaCopy");
+        document.body.appendChild(createTextarea);
+        let textareaElm = document.getElementById('textareaCopy');
+        textareaElm.value = value;
+        textareaElm.select();
+        textareaElm.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        textareaElm.remove();
+    }
     $(function () {
         handleSliderHero();
+        $(document).on('click', '.copy-value', function () {
+            if ($(this).attr('data-value') != undefined) {
+                initClipboardCopy($(this).attr('data-value'));
+            } else {
+                initClipboardCopy($(this).parent().find('input').val());
+            }
+        });
     });
 })(jQuery);
